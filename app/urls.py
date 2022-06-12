c6f306f7c1896ae43c934b1bdab8abea140dd0be
kleitich <kevleitich@gmail.com>
2022-06-12 23:43:25 +0300

    adding register pages and urls and views

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,5 @@ from django.urls import  path
 
 urlpatterns=[
     path('',views.home,name='home'),
+    path("register", views.register_request, name="register"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import login
+from django.contrib import messages
+from app.forms import NewUserForm
 
 # Create your views here.
 def home(request):
     return render(request,'index.html')
+
+def register_request(request):
+	if request.method == "POST":
+		form = NewUserForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			login(request, user)
+			messages.success(request, "Registration successful." )
+			return redirect("main:homepage")
+		messages.error(request, "Unsuccessful registration. Invalid information.")
+	form = NewUserForm()
+	return render (request=request, template_name="auth/register.html", context={"register_form":form})
