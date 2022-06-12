6d753ed0bf78d84df8297870129eb41367617806
kleitich <kevleitich@gmail.com>
2022-06-13 01:05:13 +0300

    adding profile

--- app/forms.py
+++ app/forms.py
@@ -3,6 +3,8 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+from app.models import Profile
+
 
 # Create your forms here.
 
@@ -12,3 +14,8 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class UpdateUserProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('fname','lname','bio','ppic','phone')
--- app/models.py
+++ app/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+class Profile(models.Model):
+    ppic=models.ImageField(upload_to ='ppic') 
+    bio=models.TextField(max_length=600, default="Bio")
+    phone=models.TextField(max_length=600, default="Bio")
+    fname=models.CharField(max_length=30)
+    lname=models.CharField(max_length=30)
+    user=models.OneToOneField(User, on_delete=models.CASCADE,primary_key=True)
+
+    def __str__(self):
+        return f'{self.user.username} insta-profile'
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+
+    def delete_profile(self):
+        self.delete()
+    
+    @classmethod
+    def filter_profile_by_id(cls, id):
+        profile = Profile.objects.filter(user__id = id).first()
+        return profile
+
+    @classmethod
+    def search_profile(cls, name):
+        return cls.objects.filter(user__username__icontains=name).all()
+
